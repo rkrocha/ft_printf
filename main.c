5fd60e60137147c45d14a1dc3c153dbf0ba39a31
rkochhan <rkochhan@student.42sp.org.br>
2020-10-27 13:34:20 -0300

    working makefile, repo configs

---
+++ main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/12 11:25:29 by rkochhan          #+#    #+#             */
+/*   Updated: 2020/10/13 15:00:12 by rkochhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+int ft_printf(const char *format, ...);
+malloc, free, write, va_start, va_arg, va_copy, va_end
+no buffer management , writes to stdout
+
+%[parameter][flags][width][.precision][length]type
+no parameter
+		cspdiuxX%
+flags	-0.*	minimum field width with all conversions
+
+BONUS
+conversions	nfge
+some flags:	l ll h hh
+all flags:	# " " +
+*/
+
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("");
+}
