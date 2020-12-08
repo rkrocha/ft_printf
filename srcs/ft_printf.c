bb1c4740f34363f7b685c348a70fbf2f196df797
rkochhan <rkochhan@student.42sp.org.br>
2020-12-07 23:20:51 -0300

    added compilation script and organized makefile

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -1,34 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/12 11:25:29 by rkochhan          #+#    #+#             */
-/*   Updated: 2020/12/07 21:59:09 by rkochhan         ###   ########.fr       */
+/*   Created: 2020/10/28 10:16:26 by rkochhan          #+#    #+#             */
+/*   Updated: 2020/10/28 10:34:41 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-int ft_printf(const char *format, ...);
-malloc, free, write, va_start, va_arg, va_copy, va_end
-no buffer management , writes to stdout
+#include "ft_printf.h"
 
-%[parameter][flags][width][.precision][length]type
-no parameter
-		cspdiuxX%
-flags	-0.*	minimum field width with all conversions
-
-BONUS
-conversions	nfge
-some flags:	l ll h hh
-all flags:	# " " +
-*/
-
-#include <stdio.h>
-
-int	main(void)
-{
-	printf("%.3f abc\n", 3.14);
-}
