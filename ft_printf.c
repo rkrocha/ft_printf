62d3b991c366f413868ff23d35cff1e16521720e
rkochhan <rkochhan@student.42sp.org.br>
2021-03-08 12:13:27 -0300

    finished general layout of ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,26 +6,47 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 10:16:26 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/03/07 19:11:25 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/03/08 12:04:04 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static void	branch_by_specifier(t_params *conv, va_list arg, int *nprint)
+{
+	// if ((*conv).specifier == 'c' || (*conv).specifier == '%')
+	// 	printf_char(conv, arg, nprint);
+	// else if ((*conv).specifier == 's')
+	// 	printf_str(conv, arg, nprint);
+	// else if ((*conv).specifier == 'p')
+	// 	printf_ptr(conv, arg, nprint);
+	if ((*conv).specifier == 'd' || (*conv).specifier == 'i')
+		printf_int(conv, arg, nprint);
+	// else if ((*conv).specifier == 'u')
+	// 	printf_uint(conv, arg, nprint);
+	// else if ((*conv).specifier == 'x' || (*conv).specifier == 'X')
+	// 	printf_hex(conv, arg, nprint);
+	return ;
+}
+
 static void	get_conversion(const char *format, va_list arg, int *nprint, int *i)
 {
-	t_conversion	conv;
+	t_params	conv;
 
-	(void)arg;
 	ft_bzero(&conv, sizeof(conv));
-	if (!copy_conv_spec(format, &conv, i))
+	if (copy_conversion(format, &conv, i))
 	{
-		*nprint = -1;
+		get_flags(&conv);
+		// get_bonus_flags?
+		get_width_precision(&conv, arg);
+		// get_bonus_length?
+		// if (check_common_errors)?
+			// break ;
+		// else
+		branch_by_specifier(&conv, arg, nprint);
 		return ;
 	}
-	get_flags(&conv);
-	// get_bonus_flags?
-	// get_width_precision(&conv);
+	*nprint = -1;
 	return ;
 }
 
