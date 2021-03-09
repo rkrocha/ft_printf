e09d53e47b18bbf9c1c476161fc928a8449264d0
rkochhan <rkochhan@student.42sp.org.br>
2021-03-09 20:08:46 -0300

    made code up to norme

--- ft_printf_int.c
+++ ft_printf_int.c
@@ -6,60 +6,13 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 12:53:26 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/03/09 20:01:36 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/03/09 20:06:42 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-/*
-`0' (zero)   Zero padding.  For all conversions except n, the converted value
-is padded on the left with zeros rather than blanks.  If a precision is given
-with a numeric conversion (d, i, o, u, i, x, and X), the 0 flag is ignored.
-
-`-' A negative field width flag; the converted value is to be left adjusted
-on the field boundary.  Except for n conversions, the converted value is padded
-on the right with blanks, rather than on the left with blanks or zeros.
-A - overrides a 0 if both are given.
-
-o   An optional decimal digit string specifying a minimum field width.
-If the converted value has fewer characters than the field width, it will be
-padded with spaces on the left (or right, if the left-adjustment flag has been
-given) to fill out the field width.
-
-o   An optional precision, in the form of a period . followed by an optional
-digit string.  If the digit string is omitted, the precision is taken as zero.
-This gives the minimum number of digits to appear for d, i, o, u, x, and X
-conversions, the number of digits to appear after the decimal-point for a, A, e, E, f, and F conversions, the maximum number of significant digits for g and G
-conversions, or the maximum number of characters to be printed from a string
-for s conversions.
-
-error: flag '0' is ignored when flag '-' is present [-Werror,-Wformat]
-error: '0' flag ignored with precision and ‘%d’ gnu_printf format [-Werror=format=]
-*/
-
-	// if ((*conv).precision >= (*conv).width)
-	// {
-	// 	if (num_neg)
-	// 		ft_putchar('-');
-	// 	printf_pad('0', (*conv).precision - num_len);
-	// }
-	// else if ((*conv).width > (*conv).precision)
-	// {
-	// 	if (!(*conv).flag_minus)
-	// 		printf_pad(' ', (*conv).width - (*conv).precision - num_len);
-	// 	else
-	// 	{
-	// 		if (num_neg)
-	// 			ft_putchar('-');
-	// 		printf_pad('0', (*conv).precision - num_len);
-	// 	}
-	// }
-	// ft_putnbr(num);
-	// if ((*conv).width > (*conv).precision && (*conv).flag_minus)
-	// 	printf_pad(' ', (*conv).width - (*conv).precision - num_len);
-
-static void	printf_int_print(t_params *conv, int num, int num_len, int print_len)
+static void	put_printf_int(t_params *conv, int num, int num_len, int print_len)
 {
 	bool	num_neg;
 
@@ -83,9 +36,7 @@ static void	printf_int_print(t_params *conv, int num, int num_len, int print_len
 	if ((*conv).flag_minus && (*conv).precision &&
 											(*conv).width >= (*conv).precision)
 		printf_pad(' ', (*conv).width - (*conv).precision - 1);
-		// printf_pad(' ', print_len - (*conv).precision - num_len);
 }
-	// lacks spaces after number when wid > prec
 
 static int	printf_int_len(t_params *conv, int num_len)
 {
@@ -126,5 +77,5 @@ void		printf_int(t_params *conv, va_list ap, int *nprint)
 	if (print_len == num_len && num > 0)
 		ft_putnbr(num);
 	else
-		printf_int_print(conv, num, num_len, print_len);
+		put_printf_int(conv, num, num_len, print_len);
 }
