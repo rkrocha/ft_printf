69824737544a1801515aaace0283b815dc759e17
rkochhan <rkochhan@student.42sp.org.br>
2021-03-11 16:59:09 -0300

    finished the refreshingly painless char and % conversions

--- ft_printf_char.c
+++ ft_printf_char.c
@@ -6,20 +6,18 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 14:10:44 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/03/11 03:05:49 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/03/11 16:58:17 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	printf_put_char(t_params *conv, unsigned char c, int *nprint)
-{
-	(void)conv;
-	(void)nprint;
-	ft_putchar(c);
-}
+/*
+** Precision and '0' flags result in undefined behavior with c conversion
+** '0' flag is ignored when '-' flag is present.
+*/
 
-void	printf_char(t_params *conv, va_list ap, int *nprint)
+void		printf_char(t_params *conv, va_list ap, int *nprint)
 {
 	unsigned char	c;
 
@@ -33,6 +31,11 @@ void	printf_char(t_params *conv, va_list ap, int *nprint)
 	if ((*conv).specifier == 'c')
 	{
 		c = (unsigned char)va_arg(ap, int); // treat as int to suppress error?
-		printf_put_char(conv, c, nprint);
+		*nprint += (*conv).width;
+		if (!(*conv).flag_minus && (*conv).width > 1)
+			printf_pad(' ', (*conv).width - 1);
+		ft_putchar(c);
+		if ((*conv).flag_minus && (*conv).width > 1)
+			printf_pad(' ', (*conv).width - 1);
 	}
 }
