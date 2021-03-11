dd5567d259e8b83369354090b35c7dd45755021f
rkochhan <rkochhan@student.42sp.org.br>
2021-03-11 13:07:17 -0300

    function now prints "(null)" when NULL is sent as argument in string conversion

--- ft_printf_str.c
+++ ft_printf_str.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 02:21:28 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/03/11 04:38:41 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/03/11 12:59:19 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,28 @@ static void	printf_put_str(t_params *conv, char *str, size_t len)
 {
 	if (!(*conv).flag_minus)
 	{
-		if ((*conv).precision > 0 && (size_t)(*conv).precision < len)
+		if ((*conv).flag_precision && (*conv).width > (*conv).precision &&
+												(size_t)(*conv).precision < len)
 			printf_pad(' ', (*conv).width - (*conv).precision);
 		else if ((size_t)(*conv).width > len)
 			printf_pad(' ', (*conv).width - len);
 	}
-	if ((*conv).flag_precision)
-		ft_putnstr(str, (*conv).precision);
-	else
-		ft_putstr(str);
-	if ((*conv).flag_minus && (size_t)(*conv).width > len)
-		printf_pad(' ', (*conv).width - len);
+	if (str)
+	{
+		if ((*conv).flag_precision)
+			ft_putnstr(str, (*conv).precision);
+		else
+			ft_putstr(str);
+	}
+	else if (!(*conv).flag_precision || (*conv).precision >= 6)
+		ft_putstr("(null)");
+	if ((*conv).flag_minus)
+	{
+		if ((*conv).flag_precision && (size_t)(*conv).precision < len)
+			printf_pad(' ', (*conv).width - (*conv).precision);
+		else if ((size_t)(*conv).width > len)
+			printf_pad(' ', (*conv).width - len);
+	}
 }
 
 void		printf_str(t_params *conv, va_list ap, int *nprint)
@@ -37,7 +48,12 @@ void		printf_str(t_params *conv, va_list ap, int *nprint)
 
 	// printf_str_errors?
 	str = va_arg(ap, char *);
-	len = ft_strlen(str);
+	if (str != NULL)
+		len = ft_strlen(str);
+	else if (str == NULL && (!(*conv).flag_precision || (*conv).precision >= 6))
+		len = ft_strlen("(null)");
+	else
+		len = 0;
 	if ((*conv).flag_precision && (size_t)(*conv).precision < len)
 		print_len = (*conv).precision;
 	if ((size_t)(*conv).width > print_len)
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 20:04:03 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/03/09 19:34:55 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/03/11 11:58:39 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,55 +61,6 @@ all flags:	# " " +
 
 int	main(void)
 {
-	printf("%s", "abc %5d ft\n");
-	ft_printf("abc %5d ft\n", -33);
-	printf("abc %5d def\n\n", -33);
-
-	printf("%s", "abc %.5d ft\n");
-	ft_printf("abc %.5d ft\n", -33);
-	printf("abc %.5d def\n\n", -33);
-
-	printf("%s", "abc %-5d ft\n");
-	ft_printf("abc %-5d ft\n", -33);
-	printf("abc %-5d def\n\n", -33);
-
-	printf("%s", "abc %05d ft\n");
-	ft_printf("abc %05d ft\n", -33);
-	printf("abc %05d def\n\n", -33);
-
-	printf("%s", "abc %-6.3d ft\n");
-	ft_printf("abc %-6.3d ft\n", -33);
-	printf("abc %-6.3d def\n\n", -33);
-
-	printf("%s", "abc %-3.6d ft\n");
-	ft_printf("abc %-3.6d ft\n", -33);
-	printf("abc %-3.6d def\n\n", -33);
-
-	printf("%s", "abc %-3.3d ft\n");
-	ft_printf("abc %-3.3d ft\n", -33);
-	printf("abc %-3.3d def\n\n\n\n", -33);
-
-
-
-	ft_printf("abc %5d ft\n", 33);
-	printf("abc %5d def\n\n", 33);
-
-	ft_printf("abc %.5d ft\n", 33);
-	printf("abc %.5d def\n\n", 33);
-
-	ft_printf("abc %-5d ft\n", 33);
-	printf("abc %-5d def\n\n", 33);
-
-	ft_printf("abc %05d ft\n", 33);
-	printf("abc %05d def\n\n", 33);
-
-	printf("%s", "abc %-6.3d ft\n");
-	ft_printf("abc %-6.3d ft\n", 33);
-	printf("abc %-6.3d def\n\n", 33);
-
-	ft_printf("abc %-3.6d ft\n", 33);
-	printf("abc %-3.6d def\n\n", 33);
-
-	ft_printf("abc %-3.3d ft\n", 33);
-	printf("abc %-3.3d def\n\n", 33);
+	ft_printf("abc %10s ft\n", "hi low");
+	printf("abc %10s def\n\n", "hi low");
 }
