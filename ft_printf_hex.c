d9969aa6e43824636cc5af52b12b1651c26dc397
rkochhan <rkochhan@student.42sp.org.br>
2021-03-14 16:16:51 -0300

    cleaned up ft_printf_hex.c

--- ft_printf_hex.c
+++ ft_printf_hex.c
@@ -6,50 +6,11 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 10:50:43 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/03/14 15:37:34 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/03/14 16:16:31 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-// static void	printf_put_hex(t_params conv, unsigned int num, int num_len, char *base)
-// {
-// 	if (!conv.flag_minus)
-// 	{
-// 		if (conv.precision >= num_len)
-// 			printf_pad(' ', conv.width - conv.precision);
-// 		else if (!conv.flag_zero)
-// 			printf_pad(' ', conv.width - num_len);
-// 	}
-// 	if (conv.precision >= num_len)
-// 		printf_pad('0', conv.precision - num_len);
-// 	else if (conv.flag_zero && conv.width > num_len)
-// 		printf_pad('0', conv.width - num_len);
-// 	if (!(num == 0 && conv.flag_precision && conv.precision == 0))
-// 		ft_putlnbr_base(num, base, ft_strlen(base));
-// 	else if (num == 0 && conv.width > 0)
-// 		ft_putchar(' ');
-// 	if (conv.flag_minus)
-// 	{
-// 		if (conv.width > num_len && conv.precision > num_len)
-// 			printf_pad(' ', conv.width - conv.precision);
-// 		else if (conv.width > num_len)
-// 			printf_pad(' ', conv.width - num_len);
-// 	}
-// }
-
-// static int	count_hex_digits(unsigned int num)
-// {
-// 	int	len;
-
-// 	len = 1;
-// 	while (num >= 16)
-// 	{
-// 		num /= 16;
-// 		len++;
-// 	}
-// 	return (len);
-// }
 
 static bool	printf_hex_errors(t_params *conv, int *nprint)
 {
