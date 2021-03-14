1432f07546c2a2bb9d58cf93b9389d48af15bdc6
rkochhan <rkochhan@student.42sp.org.br>
2021-03-14 15:06:22 -0300

    fixed printf_str

--- ft_printf_str.c
+++ ft_printf_str.c
@@ -6,34 +6,11 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 02:21:28 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/03/14 14:57:09 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/03/14 15:05:44 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-// static void	printf_put_str(t_params conv, char *str, size_t len)
-// {
-// 	if (!conv.flag_minus)
-// 	{
-// 		if (conv.flag_precision && conv.width > conv.precision &&
-// 												(size_t)conv.precision < len)
-// 			printf_pad(' ', conv.width - conv.precision);
-// 		else if ((size_t)conv.width > len)
-// 			printf_pad(' ', conv.width - len);
-// 	}
-// 	if (conv.flag_precision)
-// 		ft_putnstr(str, conv.precision);
-// 	else
-// 		ft_putstr(str);
-// 	if (conv.flag_minus)
-// 	{
-// 		if (conv.flag_precision && (size_t)conv.precision < len)
-// 			printf_pad(' ', conv.width - conv.precision);
-// 		else if ((size_t)conv.width > len)
-// 			printf_pad(' ', conv.width - len);
-// 	}
-// }
 
 void		printf_str(t_params *conv, va_list ap, int *nprint)
 {
@@ -43,7 +20,7 @@ void		printf_str(t_params *conv, va_list ap, int *nprint)
 	// printf_str_errors?
 	temp = va_arg(ap, char *);
 	if ((*conv).precision < 0)
-		(*conv).precision *= -1;
+		(*conv).flag_precision = false;
 	if (temp == NULL)
 	{
 		if ((*conv).flag_precision)
@@ -58,8 +35,8 @@ void		printf_str(t_params *conv, va_list ap, int *nprint)
 		else
 			(*conv).string = ft_strdup(temp);
 	}
-	len = ft_strlen((*conv).string);
 	(*conv).precision = 0;
+	len = ft_strlen((*conv).string);
 
 	*nprint += 1;
 	printf_print(*conv, len, false, false);
