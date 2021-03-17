483b4ad7684a3c1a32cf1e1298a3f29561acba3b
rkochhan <rkochhan@student.42sp.org.br>
2021-03-17 08:29:50 -0300

    fixed %% conversion to accept both precision and zero flag

--- ft_printf_conversion.c
+++ ft_printf_conversion.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 21:02:12 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/03/17 08:21:22 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/03/17 08:28:22 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ static void	adjust_flags(t_params *conv)
 	}
 	if ((*conv).flag_minus && (*conv).flag_zero)
 		(*conv).flag_zero = false;
-	if ((*conv).flag_zero && (*conv).flag_precision && (*conv).precision >= 0)
+	if ((*conv).flag_zero && (*conv).specifier != '%' &&
+							(*conv).flag_precision && (*conv).precision >= 0)
 		(*conv).flag_zero = false;
 }
 
